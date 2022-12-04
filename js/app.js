0a5f8ff4604a6ae118ece7bf0cef1f2261a8d5c7
Alejandro Cerezo <alce65@hotmail.es>
2022-12-04 11:32:04 +0100

    Solve eclint bugs

--- js/app.js
+++ js/app.js
@@ -1,16 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 const a = 33;
 const foo = function () {
-  const z = 22;
-  console.log(this.z);
+    const z = 22;
+    console.log(this.z);
 };
 foo();
 setTimeout(foo, 1);
 console.clear();
 const fooA = () => {
-  const z = 22;
-  console.log(this); // Lexical scope
+    const z = 22;
+    console.log(this); // Lexical scope
 };
 fooA();
 setTimeout(fooA, 1);
-
-55; //
