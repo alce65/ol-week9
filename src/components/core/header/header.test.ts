461a0e8b3280e0221b4dcffcbe6b9ae8b4ca4e23
Alejandro Cerezo <alce65@hotmail.es>
2022-12-04 11:25:55 +0100

    Update header test

--- src/components/core/header/header.test.ts
+++ src/components/core/header/header.test.ts
@@ -6,30 +6,31 @@ import { Header } from './header';
 describe('Given "Header" component', () => {
     document.body.innerHTML = `<slot></slot>`;
     const header = new Header('slot');
-    const elements = [
-        screen.getByRole('banner'), // <header />
-        screen.getByRole('heading', { name: 'Learning Components' }), // <h1>
-    ];
     test('Then we should to be able to instantiate it', () => {
         expect(header).toBeInstanceOf(Header);
     });
-    describe.each(elements)(
-        'When it is instantiate with a DOM selector',
-        (element: HTMLElement) => {
-            test(`Then ${element.tagName} should be render`, () => {
-                expect(element).toBeInstanceOf(HTMLElement);
-                expect(element).toBeInTheDocument();
-            });
-        }
-    );
-});
 
-describe(`Given "Header" component, 
-    When it is instantiate with a invalid DOM selector`, () => {
-    test('No element should be render and an error should be throw', () => {
-        expect(() => {
-            const result = new Header('.invalid');
-            result.render();
-        }).toThrowError('Invalid selector');
+    describe('When it is instantiate with a DOM selector', () => {
+        const elements = [
+            screen.getByRole('banner'), // <header />
+            screen.getByRole('heading', { name: 'Learning Components' }), // <h1>
+        ];
+        const toRender = elements.map((item) => ({
+            tag: item.tagName,
+            element: item,
+        }));
+
+        test.each(toRender)(`Then $tag should be render`, ({ element }) => {
+            expect(element).toBeInstanceOf(HTMLElement);
+            expect(element).toBeInTheDocument();
+        });
+    });
+    describe(`When it is instantiate with a invalid DOM selector`, () => {
+        test('No element should be render and an error should be throw', () => {
+            expect(() => {
+                const result = new Header('.invalid');
+                result.render();
+            }).toThrowError('Invalid selector');
+        });
     });
 });
